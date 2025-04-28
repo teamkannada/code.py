79644ade8b2e07f39d2e395aa52a7818a2e3816e
teamkannada <badamimanjunath5@gmail.com>
2025-04-28 23:22:46 +0530

    Update exp15.py

--- exp15.py
+++ exp15.py
@@ -1,16 +1,20 @@
-person={
-    'name':'dr.sagar',
-    'age' : 35,
+person = {
+    'name': 'dr.sagar',
+    'age': 35,
     'city': 'dharwad',
-    'college':'Gfgc Dharwad',
+    'college': 'Gfgc Dharwad',
     'position': 'Assistant professor'
 }
+
 print(person['name'])
-person['age']=36
+
+person['age'] = 36
 print(person['age'])
-person['department']='computer science'
-if 'city'
-in person:
+
+person['department'] = 'computer science'
+
+if 'city' in person:
     print(person['city'])
+
 for key, value in person.items():
-    print(f"{key}:{value}")
+    print(f"{key}: {value}")
