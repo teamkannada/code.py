1093ab4bc34fe68db7ab7805858898fbbbfb116a
teamkannada <badamimanjunath5@gmail.com>
2025-05-16 20:31:23 +0530

    Update exp16.py

--- exp16.py
+++ exp16.py
@@ -1,26 +1,35 @@
-import sqlite3 #import the sdlite3 module
+import sqlite3  # Import the sqlite3 module
+
 conn = sqlite3.connect("gfgc.db")
 cursor = conn.cursor()
-cursor.execute("DROp TABLE IF EXISTS indians")
-cursor.execute("CREATE TABLE indians (id INTEGER PRIMARY KEY,name TEXT,age INTEGER)")
-cursor.execute("INSERT into indians(name,age)VALUES('ram',20)")
-cursor.execute("INSERT INTO indians(name,age)VALUES('priya',18)")
-cursor.execute("INSERT INTO indians(name,age)VALUES('ravi',21)")
+
+cursor.execute("DROP TABLE IF EXISTS indians")
+
+cursor.execute("CREATE TABLE indians (id INTEGER PRIMARY KEY, name TEXT, age INTEGER)")
+cursor.execute("INSERT INTO indians(name, age) VALUES ('ram', 20)")
+cursor.execute("INSERT INTO indians(name, age) VALUES ('priya', 18)")
+cursor.execute("INSERT INTO indians(name, age) VALUES ('ravi', 21)")
 conn.commit()
-print("data after insert: ")
+
+print("Data after insert:")
 for row in cursor.execute("SELECT * FROM indians"):
     print(row)
 
-cursor.execute("UPDATE indians SET age=26 WHERE name='priya'")
-print("\ndata after update: ")
+cursor.execute("UPDATE indians SET age = 26 WHERE name = 'priya'")
+
+print("\nData after update:")
 for row in cursor.execute("SELECT * FROM indians"):
     print(row)
 
 cursor.execute("DELETE FROM indians WHERE name = 'ravi'")
 conn.commit()
 
-print("\ndata after delete: ")
+print("\nData after delete:")
 for row in cursor.execute("SELECT * FROM indians"):
     print(row)
 
-print("\n table drop")
+cursor.execute("DROP TABLE indians")
+conn.commit()
+print("\nTable dropped")
+
+conn.close()
